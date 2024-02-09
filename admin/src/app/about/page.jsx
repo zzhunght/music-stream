0dcd135fa345b66b40593353b95ad91cc22e511b
minhhoanq <mh.tranminhhoang@gmail.com>
2024-02-09 22:47:03 +0700

    DataTable, Pagination

--- admin/src/app/about/page.jsx
+++ admin/src/app/about/page.jsx
@@ -2,8 +2,31 @@ import React from 'react'
 import { DataTable } from './data-table'
 import { columns } from "./columns"
 import TitlePage from '@/components/titlepage/TitlePage'
+import { Pagination } from '@/components/ui/pagination'
+import { Button } from '@/components/ui/button'
 
 const data = [ 
+  {
+    no: "m5gr84i9",
+    name: 316,
+    artists: "success",
+    duration: "ken99@yahoo.com",
+    releasedate: "ken99@yahoo.com",
+  },
+  {
+    no: "m5gr84i9",
+    name: 316,
+    artists: "success",
+    duration: "ken99@yahoo.com",
+    releasedate: "ken99@yahoo.com",
+  },
+  {
+    no: "m5gr84i9",
+    name: 316,
+    artists: "success",
+    duration: "ken99@yahoo.com",
+    releasedate: "ken99@yahoo.com",
+  },
   {
     no: "m5gr84i9",
     name: 316,
@@ -47,6 +70,11 @@ function About() {
     <div>
       <TitlePage title={'Songs'}/>
       <div className="container mx-auto py-10">
+        <div>
+          <Button variant='outline' className='mb-10 border-gray-600'>
+            Add Songs
+          </Button>
+        </div>
         <DataTable columns={columns} data={data} />
       </div>
     </div>
